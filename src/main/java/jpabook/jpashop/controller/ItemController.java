5a858920d6f770d4110708aac477d891564ceff7
hschoi7 <chs4469@gmail.com>
2022-10-27 19:43:30 +0900

    상품 목록 기능

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -38,46 +38,14 @@ public class ItemController {
         return "redirect:/";
     }
 
-    
-    
+    @GetMapping("/items")
+    public String list(Model model) {
 
-    
-    
-    
-    
-
-    
-    
-    
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
 
 
-    
-    
-    
-    
-    
-    
-    
-
-    
-    
-    
-
-    
-    
-
-//  
-//  
-//  
-//  
-//  
-//  
-//  
-//
-//  
-
-    
-    
-    
 
 }
